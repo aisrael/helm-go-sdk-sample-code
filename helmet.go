dd5911d6ea739702ff3a867fc3c023c486bb39fc
Alistair Israel <aisrael@gmail.com>
2020-12-03 15:04:30 +0800

    Works

--- helmet.go
+++ helmet.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
 )
 
@@ -15,16 +16,34 @@ func main() {
 	// You can pass an empty string instead of settings.Namespace() to list
 	// all namespaces
 	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
-		log.Printf("%+v", err)
+		log.Fatalf("%+v", err)
 		os.Exit(1)
 	}
 
-	client := action.NewList(actionConfig)
-	// Only list deployed
-	client.Deployed = true
-	results, err := client.Run()
+	chart, err := loader.LoadDir("helm/charts/nginx")
 	if err != nil {
-		log.Printf("%+v", err)
+		log.Fatalf("%+v", err)
+		os.Exit(1)
+	}
+
+	install := action.NewInstall(actionConfig)
+	install.ReleaseName = "nginx"
+	vals := make(map[string]interface{})
+	release, err := install.Run(chart, vals)
+	if err != nil {
+		log.Fatalf("%+v", err)
+		os.Exit(1)
+	}
+
+	log.Printf("release.Name: %s\n", release.Name)
+	log.Printf("release.Version: %d\n", release.Version)
+
+	list := action.NewList(actionConfig)
+	// Only list deployed
+	list.Deployed = true
+	results, err := list.Run()
+	if err != nil {
+		log.Fatalf("%+v", err)
 		os.Exit(1)
 	}
 
